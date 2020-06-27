15f06a9b5c09491e8a74af8c14f61c3987f97eeb
Andy Flisher <af@xyroh.com>
2020-06-27 17:46:57 +0100

    Added BaseViewModel, start of main page layout #13

--- WorstUrlShortener/WorstUrlShortener/Views/DefaultPage.xaml.cs
+++ WorstUrlShortener/WorstUrlShortener/Views/DefaultPage.xaml.cs
@@ -1,9 +1,10 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using com.xyroh.lib;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -14,7 +15,33 @@ namespace WorstUrlShortener.Views
     {
         public DefaultPage()
         {
+            XyrohLib.LogEvent("Page : Shorten ");
+
+            this.BindingContext = App.ViewModel;
             this.InitializeComponent();
+
+            URLShortener.SelectedIndex = 0;
+        }
+
+
+        private async void OnShortenButtonClicked(object sender, EventArgs e)
+        {
+            try
+            {
+                XyrohLib.LogEvent("Shorten Page : Send Ticket");
+
+                ShortenButton.IsEnabled = false;
+
+                ShortURL.IsVisible = true;
+                ShortURL.Text = URLShortener.SelectedItem.ToString() + " - " + FullURL.Text;
+            }
+            catch (Exception ex)
+            {
+                XyrohLib.LogCrash(ex);
+
+                await this.DisplayAlert("Error", "Error: " + ex.Message, "OK");
+                this.ShortenButton.IsEnabled = true;
+            }
         }
     }
 }
