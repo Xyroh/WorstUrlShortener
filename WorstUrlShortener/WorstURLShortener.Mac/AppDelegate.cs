268ce40d974b98ca025859c85bf41a1908e961b0
Andy Flisher <af@xyroh.com>
2020-07-24 11:15:15 +0100

    core packages etc

--- WorstUrlShortener/WorstURLShortener.Mac/AppDelegate.cs
+++ WorstUrlShortener/WorstURLShortener.Mac/AppDelegate.cs
@@ -1,5 +1,8 @@
-﻿using AppKit;
+﻿using System;
+using System.Threading.Tasks;
+using AppKit;
 using Foundation;
+using com.xyroh.lib;
 
 namespace WorstURLShortener.Mac
 {
@@ -13,11 +16,27 @@ namespace WorstURLShortener.Mac
         public override void DidFinishLaunching(NSNotification notification)
         {
             // Insert code here to initialize your application
+            AppDomain.CurrentDomain.UnhandledException += UnhandledExceptionHandler;
+            TaskScheduler.UnobservedTaskException += UnobservedTaskExceptionHandler;
+
         }
 
         public override void WillTerminate(NSNotification notification)
         {
             // Insert code here to tear down your application
+        }
+
+        static void UnhandledExceptionHandler(object sender, UnhandledExceptionEventArgs e)
+        {
+            var ex = (Exception)e.ExceptionObject;
+            XyrohLib.LogException("Unhandled Exception", ex);
+            Environment.Exit(1);
+        }
+
+        static void UnobservedTaskExceptionHandler(object sender, UnobservedTaskExceptionEventArgs e)
+        {
+            var ex = (Exception)e.Exception;
+            XyrohLib.LogException("Unobserved Task Exception", ex);
         }
     }
 }
