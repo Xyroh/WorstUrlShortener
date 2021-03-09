0b14a0d232cf243ede1e1561db60ac6dec198aba
Andy Flisher <af@xyroh.com>
2021-03-09 17:02:16 +0000

    History Page #44 - loads from viewmodel
    Signed-off-by: Andy Flisher <af@xyroh.com>

--- WorstUrlShortener/WorstUrlShortener/ViewModels/ShortenViewModel.cs
+++ WorstUrlShortener/WorstUrlShortener/ViewModels/ShortenViewModel.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,7 @@ using Newtonsoft.Json;
 using WorstUrlShortener.DAO;
 using WorstUrlShortener.Models;
 using WorstUrlShortener.Models.Json;
+using WorstUrlShortener.Utilities;
 using Xamarin.Essentials;
 using Xamarin.Forms;
 
@@ -26,6 +28,220 @@ namespace WorstUrlShortener.ViewModels
         private bool hasResults = false;
 
         private SQLiteContext db;
+
+        private ObservableRangeCollection<ShortenedUrl> history = new ObservableRangeCollection<ShortenedUrl>();
+
+        private Command refreshCmd;
+
+        private async Task refresh()
+        {
+            await ExecuteRefreshCommand();
+        }
+
+        private async Task ExecuteRefreshCommand()
+        {
+            XyrohLib.LogEvent("Refresh History", "DB");
+
+
+            if (IsBusy)
+            {
+                XyrohLib.Log("VM BUSY - RETURNING");
+                return;
+            }
+            IsBusy = true;
+            try
+            {
+                var record = await this.GetHistory();
+
+            }
+
+            catch (Exception ex)
+            {
+                XyrohLib.LogCrash("DB", ex);
+
+            }
+            finally
+            {
+                IsBusy = false;
+            }
+        }
+
+        private async Task<ObservableRangeCollection<ShortenedUrl>> GetHistory()
+        {
+            this.IsBusy = true;
+            this.LastError = string.Empty;
+
+
+            try
+            {
+                this.History.Clear();
+
+                var records = this.db.ShortenedUrl.ToList();
+                records = records.OrderByDescending(x => x.CreatedAt).ToList();
+
+                this.History.ReplaceRange(records);
+            }
+            catch (Exception ex)
+            {
+                this.LastError = "ERROR: " + ex.Message;
+                XyrohLib.LogException("GetHistory", ex);
+            }
+
+            this.IsBusy = false;
+
+            return this.History;
+        }
+
+        private async void OnShortenCommandExecuted(object state)
+        {
+            XyrohLib.Log("SERVICE: " + this.ShortenService);
+            XyrohLib.Log("URL: " + this.LongURL);
+
+            var shortenedURl = string.Empty;
+            this.ShortURL = shortenedURl;
+            this.HasResults = false;
+
+            switch (this.shortenService)
+            {
+                default:
+                case "TinyUrl":
+                    {
+                        var client = new HttpClient();
+                        var url = "http://tinyurl.com/api-create.php?url=" + this.LongURL;
+                        XyrohLib.Log("REQUEST: " + url);
+                        client.Timeout = TimeSpan.FromSeconds(5);
+
+                        try
+                        {
+                            var response = await client.GetAsync(url);
+                            var responseBody = await response.Content.ReadAsStringAsync();
+                            XyrohLib.Log("RESP: " + responseBody);
+                            XyrohLib.Log("Status Code: " + response.StatusCode.ToString());
+                            if (response.IsSuccessStatusCode)
+                            {
+                                shortenedURl = responseBody.ToString();
+                                XyrohLib.Log("Short: " + shortenedURl);
+                            }
+                            else
+                            {
+                                this.LastError = "Something went wrong, error: " + response.StatusCode.ToString();
+                            }
+                        }
+                        catch (Exception postEx)
+                        {
+                            XyrohLib.LogCrash(postEx);
+                            this.LastError = "We couldn't shorten that URL, please check and try again";
+                        }
+                        break;
+                    }
+                case "Firebase":
+                    {
+                        var client = new HttpClient();
+                        var url = "https://firebasedynamiclinks.googleapis.com/v1/shortLinks?key=" + BaseConfig.FirebaseAPIKey.ToString();
+                        client.Timeout = TimeSpan.FromSeconds(5);
+
+                        try
+                        {
+                            var json = new FirebaseLinkRequest();
+                            json.longDynamicLink = BaseConfig.FirebaseURLDomain + "/?link=" +
+                                                   this.LongURL + "&apn=" + AppInfo.PackageName + "&ibi=" +
+                                                   AppInfo.PackageName;
+
+                            XyrohLib.Log("JSON: " + JsonConvert.SerializeObject(json));
+
+                            var requestBody = new StringContent(JsonConvert.SerializeObject(json).ToString(), Encoding.UTF8, "application/json");
+                            var response = await client.PostAsync(url, requestBody);
+                            var responseString = await response.Content.ReadAsStringAsync();
+                            XyrohLib.Log("RESP: " + responseString);
+                            XyrohLib.Log("Status Code: " + response.StatusCode.ToString());
+                            if (response.IsSuccessStatusCode)
+                            {
+                                var responseBody = JsonConvert.DeserializeObject<FirebaseLinkResponse>(responseString);
+
+                                shortenedURl = responseBody.shortLink.ToString();
+                                XyrohLib.Log("Short: " + shortenedURl);
+                            }
+                            else
+                            {
+                                this.LastError = "Something went wrong, error: " + response.StatusCode.ToString();
+                            }
+                        }
+                        catch (Exception postEx)
+                        {
+                            XyrohLib.LogCrash(postEx);
+                            this.LastError = "We couldn't shorten that URL, please check and try again";
+                        }
+                        break;
+                    }
+            }
+
+            this.ShortURL = shortenedURl;
+            if (!string.IsNullOrEmpty(this.ShortURL))
+            {
+                // save to history
+                var history = new ShortenedUrl();
+                history.ShortenService = this.shortenService;
+                history.FullUrl = this.LongURL;
+                history.ShortUrl = this.ShortURL;
+
+                try
+                {
+                    // Can't use App singleton as App doesn't exist for the extension
+                    this.db.Add(history);
+                    await this.db.SaveChangesAsync();
+
+                    XyrohLib.Log("Saved History: " + shortenedURl);
+                }
+                catch (Exception ex)
+                {
+                    XyrohLib.LogCrash("DB", ex);
+                }
+
+                await Clipboard.SetTextAsync(shortenedURl);
+
+                this.HasResults = true;
+                XyrohLib.Log("Final: " + this.ShortURL);
+
+                await this.refresh();
+            }
+
+        }
+
+        private async void OnShareCommandExecuted(object state)
+        {
+            XyrohLib.LogEvent("Shorten Page : Share Link");
+
+            await Share.RequestAsync(new ShareTextRequest
+            {
+                Uri = this.ShortURL,
+                Title = "Shortened Link"
+            });
+        }
+
+
+        // initiated in the private def, refreshed in the constructor
+        public ObservableRangeCollection<ShortenedUrl> History
+        {
+            get
+            {
+                return this.history;
+            }
+
+            set
+            {
+                this.history = value;
+                OnPropertyChanged("History");
+            }
+        }
+
+        public Command RefreshCommand
+        {
+            get
+            {
+                return this.refreshCmd ?? (this.refreshCmd = new Command(async () => await ExecuteRefreshCommand()));
+            }
+        }
+
 
         public ICommand ShortenCommand
         {
@@ -88,130 +304,10 @@ namespace WorstUrlShortener.ViewModels
 
             this.db = new SQLiteContext();
 
-        }
-
-        private async void OnShortenCommandExecuted(object state)
-        {
-            XyrohLib.Log("SERVICE: " + this.ShortenService);
-            XyrohLib.Log("URL: " + this.LongURL);
-
-            var shortenedURl = string.Empty;
-            this.ShortURL = shortenedURl;
-            this.HasResults = false;
-
-            switch (this.shortenService)
-            {
-                default:
-                case "TinyUrl":
-                {
-                    var client = new HttpClient();
-                    var url = "http://tinyurl.com/api-create.php?url=" + this.LongURL;
-                    XyrohLib.Log("REQUEST: " + url);
-                    client.Timeout = TimeSpan.FromSeconds(5);
-
-                    try
-                    {
-                        var response = await client.GetAsync(url);
-                        var responseBody = await response.Content.ReadAsStringAsync();
-                        XyrohLib.Log("RESP: " + responseBody);
-                        XyrohLib.Log("Status Code: " + response.StatusCode.ToString());
-                        if (response.IsSuccessStatusCode)
-                        {
-                            shortenedURl = responseBody.ToString();
-                            XyrohLib.Log("Short: " + shortenedURl);
-                        }
-                        else
-                        {
-                            this.LastError = "Something went wrong, error: " + response.StatusCode.ToString();
-                        }
-                    }
-                    catch (Exception postEx)
-                    {
-                        XyrohLib.LogCrash(postEx);
-                        this.LastError = "We couldn't shorten that URL, please check and try again";
-                    }
-                    break;
-                }
-                case "Firebase":
-                {
-                    var client = new HttpClient();
-                    var url = "https://firebasedynamiclinks.googleapis.com/v1/shortLinks?key=" + BaseConfig.FirebaseAPIKey.ToString();
-                    client.Timeout = TimeSpan.FromSeconds(5);
-
-                    try
-                    {
-                        var json = new FirebaseLinkRequest();
-                        json.longDynamicLink = BaseConfig.FirebaseURLDomain + "/?link=" +
-                                               this.LongURL + "&apn=" + AppInfo.PackageName + "&ibi=" +
-                                               AppInfo.PackageName;
-
-                        XyrohLib.Log("JSON: " + JsonConvert.SerializeObject(json));
-
-                        var requestBody = new StringContent(JsonConvert.SerializeObject(json).ToString(), Encoding.UTF8, "application/json");
-                        var response = await client.PostAsync(url, requestBody);
-                        var responseString = await response.Content.ReadAsStringAsync();
-                        XyrohLib.Log("RESP: " + responseString);
-                        XyrohLib.Log("Status Code: " + response.StatusCode.ToString());
-                        if (response.IsSuccessStatusCode)
-                        {
-                            var responseBody = JsonConvert.DeserializeObject<FirebaseLinkResponse>(responseString);
-
-                            shortenedURl = responseBody.shortLink.ToString();
-                            XyrohLib.Log("Short: " + shortenedURl);
-                        }
-                        else
-                        {
-                            this.LastError = "Something went wrong, error: " + response.StatusCode.ToString();
-                        }
-                    }
-                    catch (Exception postEx)
-                    {
-                        XyrohLib.LogCrash(postEx);
-                        this.LastError = "We couldn't shorten that URL, please check and try again";
-                    }
-                    break;
-                }
-            }
-
-            this.ShortURL = shortenedURl;
-            if (!string.IsNullOrEmpty(this.ShortURL))
-            {
-                // save to history
-                var history = new ShortenedUrl();
-                history.ShortenService = this.shortenService;
-                history.FullUrl = this.LongURL;
-                history.ShortUrl = this.ShortURL;
-
-                try
-                {
-                    // Can't use App singleton as App doesn't exist for the extension
-                    this.db.Add(history);
-                    await this.db.SaveChangesAsync();
-
-                    XyrohLib.Log("Saved History: " + shortenedURl);
-                }
-                catch (Exception ex)
-                {
-                    XyrohLib.LogCrash("DB", ex);
-                }
-
-                await Clipboard.SetTextAsync(shortenedURl);
-
-                this.HasResults = true;
-                XyrohLib.Log("Final: " + this.ShortURL);
-            }
+            this.refresh();
 
         }
 
-        private async void OnShareCommandExecuted(object state)
-        {
-            XyrohLib.LogEvent("Shorten Page : Share Link");
 
-            await Share.RequestAsync(new ShareTextRequest
-            {
-                Uri = this.ShortURL,
-                Title = "Shortened Link"
-            });
-        }
     }
 }
--- WorstUrlShortener/WorstUrlShortener/Views/HistoryPage.xaml.cs
+++ WorstUrlShortener/WorstUrlShortener/Views/HistoryPage.xaml.cs
@@ -1,15 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using com.xyroh.lib;
+using WorstUrlShortener.ViewModels;
 using Xamarin.Forms;
 
 namespace WorstUrlShortener.Views
 {
     public partial class HistoryPage : ContentPage
     {
+        private ShortenViewModel viewModel = new ShortenViewModel();
+
         public HistoryPage()
         {
-            InitializeComponent();
+            XyrohLib.LogEvent("Page : History ");
+
+            this.BindingContext = this.viewModel;
+            this.InitializeComponent();
+
+
+            foreach(var hist in this.viewModel.History)
+            {
+                XyrohLib.Log("HIST: " + hist.ShortUrl);
+            }
         }
     }
 }
